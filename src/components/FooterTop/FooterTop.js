d41a33ec26d25167a7b0a515803b49b33d45d623
Vova <uladzimirfilipau@gmail.com>
2022-12-26 16:46:12 +0300

    refactor: FooterTop

--- src/components/FooterTop/FooterTop.js
+++ src/components/FooterTop/FooterTop.js
@@ -13,55 +13,57 @@ function FooterTop() {
         </p>
       </article>
 
-      <nav className='footer__links'>
-        Menu
-        <Link to='/' className='footer__link'>
-          Home
-        </Link>
-        <Link to='/about' className='footer__link'>
-          About
-        </Link>
-        <Link to='/about' className='footer__link'>
-          Blog
-        </Link>
-        <Link to='/about' className='footer__link'>
-          Blog post
-        </Link>
-        <Link to='/donate' className='footer__link'>
-          Donate
-        </Link>
-        <Link to='/donate' className='footer__link'>
-          Donate Single
-        </Link>
-        <Link to='/donate' className='footer__link'>
-          Contact
-        </Link>
-      </nav>
+      <section className='footer__links-container'>
+        <nav className='footer__links'>
+          Menu
+          <Link to='/' className='footer__link'>
+            Home
+          </Link>
+          <Link to='/about' className='footer__link'>
+            About
+          </Link>
+          <Link to='/about' className='footer__link'>
+            Blog
+          </Link>
+          <Link to='/about' className='footer__link'>
+            Blog post
+          </Link>
+          <Link to='/donate' className='footer__link'>
+            Donate
+          </Link>
+          <Link to='/donate' className='footer__link'>
+            Donate Single
+          </Link>
+          <Link to='/donate' className='footer__link'>
+            Contact
+          </Link>
+        </nav>
 
-      <nav className='footer__links'>
-        Utility Pages
-        <Link to='/' className='footer__link'>
-          Start here
-        </Link>
-        <Link to='/about' className='footer__link'>
-          Styleguide
-        </Link>
-        <Link to='/about' className='footer__link'>
-          Password Protected
-        </Link>
-        <Link to='/notfound' className='footer__link'>
-          404 Not found
-        </Link>
-        <Link to='/donate' className='footer__link'>
-          Licenses
-        </Link>
-        <Link to='/donate' className='footer__link'>
-          Changelog
-        </Link>
-      </nav>
+        <nav className='footer__links'>
+          Utility Pages
+          <Link to='/' className='footer__link'>
+            Start here
+          </Link>
+          <Link to='/about' className='footer__link'>
+            Styleguide
+          </Link>
+          <Link to='/about' className='footer__link'>
+            Password Protected
+          </Link>
+          <Link to='/notfound' className='footer__link'>
+            404 Not found
+          </Link>
+          <Link to='/donate' className='footer__link'>
+            Licenses
+          </Link>
+          <Link to='/donate' className='footer__link'>
+            Changelog
+          </Link>
+        </nav>
+      </section>
 
       <article className='footer__buttons'>
-        <p className='footer__buttons-text'>Join us and let’s make a better world, today</p>
+        <p className='footer__buttons-text'>Join us and let's make a better world, today</p>
         <Link to='/donate' className='footer__button footer__button_black'>
           Donate now
         </Link>
