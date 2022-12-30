77dbc5dcdee2f045a5921fa8a7250396cde1a29a
Vova <uladzimirfilipau@gmail.com>
2022-12-30 14:32:42 +0300

    feat: Story

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import './About.css';
-
 import Charity from '../Charity/Charity';
 import Result from '../Result/Result';
+import Story from '../Story/Story';
 
 function About() {
   return (
     <main className='main'>
       <Charity />
       <Result />
+      <Story />
     </main>
   );
 }
