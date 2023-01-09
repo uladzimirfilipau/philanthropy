2690da4db710432ffedaf783f48e313e9a1b9791
Vova <uladzimirfilipau@gmail.com>
2023-01-09 13:50:40 +0300

    add: consts

--- src/utils/consts.js
+++ src/utils/consts.js
@@ -1,1 +1,5 @@
 export const MEDIUM_SCREEN = 768;
+export const buttonLeadText = 'About our charity';
+export const buttonAboutUsText = 'About us';
+export const buttonNewsText = 'Read more news';
+export const buttonCharityText = 'How can I help';
