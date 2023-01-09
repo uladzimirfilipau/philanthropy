6920108f88c73735987577a0ab5da1fd127bc9fb
Vova <uladzimirfilipau@gmail.com>
2023-01-09 13:18:43 +0300

    refactor: AboutUs

--- src/components/AboutUs/AboutUs.jsx
+++ src/components/AboutUs/AboutUs.jsx
@@ -3,7 +3,9 @@ import './AboutUs.css';
 import aboutusImage01 from '../../images/aboutus-image-01.png';
 import aboutusImage02 from '../../images/aboutus-image-02.png';
 import aboutusImage03 from '../../images/aboutus-image-03.png';
-import { Link } from 'react-router-dom';
+import ButtonDonate from '../ButtonDonate/ButtonDonate';
+import ButtonAbout from '../ButtonAbout/ButtonAbout';
+import { buttonAboutUsText } from '../../utils/consts';
 
 function AboutUs() {
   return (
@@ -35,10 +37,10 @@ function AboutUs() {
         </li>
       </ul>
 
-      <h2 className='aboutus__title'>About us</h2>
+      <p className='aboutus__subtitle'>About us</p>
       <article className='aboutus__columns'>
-        <section className='aboutus__column '>
-          <h3 className='aboutus__subtitle'>We are here to support vulnerable communities</h3>
+        <section className='aboutus__column'>
+          <h2 className='aboutus__title'>We are here to support vulnerable communities</h2>
           <p className='aboutus__text'>
             Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt
             ut labore et dolore magna aliqua molestie a iaculis at erat pellentesque adipiscing
@@ -57,20 +59,16 @@ function AboutUs() {
         <section className='aboutus__column aboutus__column_reverse' aria-label='Links'>
           <ul className='aboutus__links'>
             <li>
-              <Link to='/donate' className='aboutus__link '>
-                Donate now
-              </Link>
+              <ButtonDonate />
             </li>
             <li>
-              <Link to='/about' className='aboutus__link aboutus__link_white'>
-                About us
-              </Link>
+              <ButtonAbout buttonText={buttonAboutUsText} />
             </li>
           </ul>
 
           <img src={aboutusImage03} alt='Children do exercises' className='aboutus__image' />
 
-          <section className='aboutus__text-container' aria-label='Description'>
+          <section className='aboutus__text-container' aria-label='Text and List'>
             <p className='aboutus__text'>
               Vitae aliquet nec ullamcorper sit amet risus nullam eget felis et netus et malesuada
               fames urna nec tincidunt praesent semper feugiat metus aliquam eleifend mi in nulla
