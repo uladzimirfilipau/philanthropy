32551a3109f9ca51f7b09c64d36ff34be55969d2
Vova <uladzimirfilipau@gmail.com>
2023-01-10 13:04:09 +0300

    refactor: FooterBottom

--- src/components/FooterBottom/FooterBottom.jsx
+++ src/components/FooterBottom/FooterBottom.jsx
@@ -12,43 +12,83 @@ function FooterBottom() {
     <section className='footer__bottom'>
       <p className='footer__copyright'>
         Copyright © Philanthropy X | Designed by&nbsp;
-        <a href='https://brixtemplates.com/' className='footer__copyright-link'>
+        <a
+          href='https://brixtemplates.com/'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='footer__copyright-link'
+        >
           BRIX Templates
         </a>
         &nbsp;| Powered by&nbsp;
-        <a href='https://github.com/uladzimirfilipau' className='footer__copyright-link'>
+        <a
+          href='https://github.com/uladzimirfilipau'
+          target='_blank'
+          rel='noopener noreferrer'
+          className='footer__copyright-link'
+        >
           Uladzimir Filipau
         </a>
       </p>
 
       <ul className='footer__icon-links'>
         <li className='footer__icon-item'>
-          <a href='https://www.facebook.com/' className='footer__icon-link'>
+          <a
+            href='https://www.facebook.com/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='footer__icon-link'
+          >
             <img src={facebookIcon} alt='Facebook' className='footer__icon-image' />
           </a>
         </li>
         <li className='footer__icon-item'>
-          <a href='https://twitter.com/' className='footer__icon-link'>
+          <a
+            href='https://twitter.com/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='footer__icon-link'
+          >
             <img src={twitterIcon} alt='Twitter' className='footer__icon-image' />
           </a>
         </li>
         <li className='footer__icon-item'>
-          <a href='https://www.instagram.com/' className='footer__icon-link'>
+          <a
+            href='https://www.instagram.com/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='footer__icon-link'
+          >
             <img src={instagramIcon} alt='Instagram' className='footer__icon-image' />
           </a>
         </li>
         <li className='footer__icon-item'>
-          <a href='https://www.linkedin.com' className='footer__icon-link'>
+          <a
+            href='https://www.linkedin.com'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='footer__icon-link'
+          >
             <img src={linkedInIcon} alt='LinkedIn' className='footer__icon-image' />
           </a>
         </li>
         <li className='footer__icon-item'>
-          <a href='https://www.youtube.com' className='footer__icon-link'>
+          <a
+            href='https://www.youtube.com'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='footer__icon-link'
+          >
             <img src={youTubeIcon} alt='YouTube' className='footer__icon-image' />
           </a>
         </li>
         <li className='footer__icon-item'>
-          <a href='https://www.whatsapp.com/' className='footer__icon-link'>
+          <a
+            href='https://www.whatsapp.com/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='footer__icon-link'
+          >
             <img src={whatsAppIcon} alt='WhatsApp' className='footer__icon-image' />
           </a>
         </li>
