962a97dc520e93eaf953463c67620515fbf3830f
Vova <uladzimirfilipau@gmail.com>
2023-01-20 21:36:42 +0300

    refactor: Questions

--- src/components/Questions/Questions.jsx
+++ src/components/Questions/Questions.jsx
@@ -4,15 +4,12 @@ import { ASKED_QUESTIONS } from '../../utils/consts';
 import Question from '../Question/Question';
 
 function Questions() {
+  const questions = ASKED_QUESTIONS.map((item) => <Question key={item.id} item={item} />);
+
   return (
     <section className='questions' aria-label='Questions'>
       <h2 className='questions__title'>Frequently Asked Questions</h2>
-
-      <ul className='questions__list'>
-        {ASKED_QUESTIONS.map((item) => (
-          <Question key={item.id} item={item} />
-        ))}
-      </ul>
+      <ul className='questions__list'>{questions}</ul>
     </section>
   );
 }
